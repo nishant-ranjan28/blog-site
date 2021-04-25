84e59cf413d760e9ff4e576e4c91a5298c1ce89e
nishant-ranjan28 <nishantranjan78@gmail.com>
2021-04-25 16:16:35 +0530

    modified title

--- content/blog/10 JavaScript Array methods to simplify your code/test.js
+++
@@ -1,9 +0,0 @@
-
-
-const techs = ['HTML', 'CSS', 'JavaScript'];
-techs.sort();
-console.log(techs); //['CSS', 'HTML', 'JavaScript']
-
-const numbers = [1, 30, 4, 21, 10000];
-numbers.sort((x, y) => x - y);
-console.log(numbers); //[1, 4, 21, 30, 10000]
